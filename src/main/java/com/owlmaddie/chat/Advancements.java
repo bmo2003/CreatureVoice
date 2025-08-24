d7442e5eb37afa36fa92da08c6f148495da50f9f
Jonathan Thomas <jonathan@openshot.org>
2025-08-24 00:19:59 -0500

    Updating advancement icons

--- src/main/java/com/owlmaddie/chat/Advancements.java
+++ src/main/java/com/owlmaddie/chat/Advancements.java
@@ -27,10 +27,10 @@ public enum Advancements {
             Items.POPPY, ICE_BREAKER, 0, false),
 
     NO_HARD_FEELINGS("no_hard_feelings", "No Hard Feelings", "Regain a friend.", Type.TASK,
-            Items.HONEY_BOTTLE, ICE_BREAKER, 0, false),
+            Items.CAKE, ICE_BREAKER, 0, false),
 
     OPEN_SESAME("open_sesame", "Open Sesame", "Your stuff is my stuff.", Type.TASK,
-            Items.TRIPWIRE_HOOK, FIRST_IMPRESSIONS, 0, false),
+            Items.CHEST, FIRST_IMPRESSIONS, 0, false),
 
     TAG_ALONG("tag_along", "Tag Along", "Follow me, bro.", Type.TASK,
             Items.LEAD, FIRST_IMPRESSIONS, 0, false),
@@ -39,25 +39,25 @@ public enum Advancements {
             Items.COMPASS, TAG_ALONG, 0, false),
 
     CALM_THE_STORM("calm_the_storm", "Calm The Storm", "Talk them down.", Type.GOAL,
-            Items.WHITE_BANNER, FIRST_IMPRESSIONS, 0, false),
+            Items.WATER_BUCKET, FIRST_IMPRESSIONS, 0, false),
 
     STAND_YOUR_GROUND("stand_your_ground", "Stand Your Ground", "Stop. It's okay.", Type.GOAL,
-            Items.CARROT_ON_A_STICK, FIRST_IMPRESSIONS, 0, false),
+            Items.OAK_SIGN, FIRST_IMPRESSIONS, 0, false),
 
     SWORN_OATH("sworn_oath", "Sworn Oath", "I will protect you.", Type.GOAL,
             Items.SHIELD, FIRST_IMPRESSIONS, 0, false),
 
     WORDSMITH("wordsmith", "Wordsmith", "From rocky start to best friends.", Type.CHALLENGE,
-            Items.WRITABLE_BOOK, NO_HARD_FEELINGS, 350, false),
+            Items.FEATHER, NO_HARD_FEELINGS, 350, false),
 
     TRUE_COMPANION("true_companion", "True Companion", "Best friends forever.", Type.GOAL,
             Items.NAME_TAG, FIRST_IMPRESSIONS, 0, false),
 
     SLEIGHT_OF_HAND("sleight_of_hand", "Sleight of Hand", "Try my sword.", Type.GOAL,
-            Items.TOTEM_OF_UNDYING, TRUE_COMPANION, 0, false),
+            Items.STICK, TRUE_COMPANION, 0, false),
 
     SHARED_STASH("shared_stash", "Shared Stash", "Share the loot.", Type.GOAL,
-            Items.CHEST, OPEN_SESAME, 0, false),
+            Items.BUNDLE, OPEN_SESAME, 0, false),
 
     SOCIAL_BUTTERFLY("social_butterfly", "Social Butterfly", "Trust blooms everywhere.", Type.GOAL,
             Items.HONEYCOMB, FIRST_IMPRESSIONS, 0, false),
@@ -69,28 +69,28 @@ public enum Advancements {
             Items.BELL, INNER_CIRCLE, 300, false),
 
     DRAMA_LLAMA("drama_llama", "Drama Llama", "Best friends for never.", Type.GOAL,
-            Items.LIGHTNING_ROD, FIRST_IMPRESSIONS, 0, false),
+            Items.TNT, FIRST_IMPRESSIONS, 0, false),
 
     LOVE_HATE_RELATIONSHIP("love_hate_relationship", "Love Hate Relationship", "It’s Complicated.", Type.CHALLENGE,
-            Items.COMPARATOR, DRAMA_LLAMA, 350, false),
+            Items.WITHER_ROSE, DRAMA_LLAMA, 350, false),
 
     ARCH_NEMESIS("arch_nemesis", "Arch Nemesis", "Meet your worst enemy.", Type.CHALLENGE,
             Items.CROSSBOW, NO_HARD_FEELINGS, 150, false),
 
     FRIEND_OR_FOE("friend_or_foe", "Friend Or Foe", "Remember the good times.", Type.CHALLENGE,
-            Items.TARGET, ARCH_NEMESIS, 400, false),
+            Items.HEART_OF_THE_SEA, ARCH_NEMESIS, 400, false),
 
     FINDERS_KEEPERS("finders_keepers", "Finder’s Keepers", "Borrowed forever.", Type.CHALLENGE,
             Items.ITEM_FRAME, TRUE_COMPANION, 250, false),
 
     GUIDED_TOUR("guided_tour", "Guided Tour", "Lost, together.", Type.GOAL,
-            Items.MAP, LEAD_THE_WAY, 0, false),
+            Items.SPYGLASS, LEAD_THE_WAY, 0, false),
 
     THE_NEVERENDING_STORY("the_neverending_story", "The NeverEnding Story", "The book is still open.", Type.GOAL,
-            Items.CAMPFIRE, ICE_BREAKER, 0, false),
+            Items.WRITABLE_BOOK, ICE_BREAKER, 0, false),
 
     GRAND_GESTURE("grand_gesture", "Grand Gesture", "A little effort, a big return.", Type.GOAL,
-            Items.DIAMOND, FIRST_IMPRESSIONS, 0, false),
+            Items.EMERALD, FIRST_IMPRESSIONS, 0, false),
 
     A_LEGEND("a_legend", "A Legend", "Blood for the crown.", Type.CHALLENGE,
             Items.GOLDEN_HELMET, TRUE_COMPANION, 500, true),
@@ -105,7 +105,7 @@ public enum Advancements {
             Items.DIAMOND, FINDERS_KEEPERS, 500, true),
 
     ENDER_ESCORT("ender_escort", "Ender Escort", "Together to the End.", Type.CHALLENGE,
-            Items.ENDER_PEARL, TAG_ALONG, 400, true);
+            Items.ENDER_EYE, TAG_ALONG, 400, true);
 
     public final ResourceLocation id;
     public final String title;
